17a3ca8d9c5104fd440402608ce8dd00fe152518
YTran4102 <ytran4102@gmail.com>
2023-05-22 22:37:01 +0700

    Done task week 6

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import './css/App.css';
 import React from 'react';
 import Chatbox from './components/Chatbox';
 import { Link } from 'react-router-dom'
+import firebase from './firebase';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: '',
-      items: [],
+      message: ''
     };
   }
   onChange = (event) => {
-    this.setState({ term: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   onSubmit = (event) => {
     event.preventDefault();
-    this.setState({
-      term: '', items: [...this.state.items, this.state.term],
-    });
+    if (this.state.message !== '') {
+      const chatRef = firebase.database().ref('general');
+      const chat = {
+        message: this.state.message,
+        user: this.props.user.displayName,
+        timestamp: new Date().getTime(),
+      };
+      chatRef.push(chat);
+      this.setState({ message: '' });
+    }
   };
   render() {
     return (
@@ -27,9 +34,12 @@ class App extends React.Component {
         <h1>Chat App</h1>
         {this.props.user && (
           <div className="allow-chat">
-            <Chatbox items={this.state.items} />
+            <Chatbox />
             <form className="message-form" onSubmit={this.onSubmit}>
-              <input value={this.state.term} onChange={this.onChange} />
+              <input
+                type="text" name="message" id="message" value={this.state.message} placeholder="Enter a message..."
+                onChange={this.onChange}
+              />
               <button>Send</button>
             </form>
           </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-
-
 import firebase from "firebase/compat/app";
 import 'firebase/compat/auth';
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import 'firebase/compat/database'
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -11,10 +9,10 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
     measurementId: "G-JYK3QCTK2M"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
